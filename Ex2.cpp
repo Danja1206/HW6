92a511545d1c605fda84a94e11444e050ad76a55
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-06-07 18:33:40 +0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<class CharT, class Traits>
+basic_ostream<CharT, Traits>& endll(basic_ostream<CharT, Traits>& ostr)
+{
+    ostr.put(ostr.widen('\n'));
+    ostr.put(ostr.widen('\n'));
+    ostr.flush();
+
+    return ostr;
+}
+
+int main()
+{
+    cout << "Тест" << endll;
+    cout << "Тест2" << endll;
+    return 0;
+}
