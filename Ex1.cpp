574207bcfdb4827cfbf81e52b317bde4297165ec
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-06-07 18:29:57 +0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a;
+    while (true)
+    {
+		cin >> a;
+
+		if (cin.good() && cin.peek() == '\n')
+		{
+			break;
+		}
+
+		cin.clear();
+		cout << "Ошибка ввода. Попробуйте ещё раз: ";
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    }
+    cout << a << endl;
+	return 0;
+}
